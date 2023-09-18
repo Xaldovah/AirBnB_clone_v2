5f13f5afce93f058042eabf07b54f8d87fc18bbe
Dennis Kimani <dxaldovah@gmail.com>
2023-09-18 10:51:00 -0700

    Update the def do_create(self, arg): function of your command interpreter (console.py) to allow for object creation with given parameters

--- models/amenity.py
+++ models/amenity.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-import uuid
 
 class Amenity(BaseModel):
-    id = ""
     name = ""
-
-    def __init__(self, *args, **kwargs):
-        """ Initialize State instance """
-        super().__init__(*args, **kwargs)
-
-        if "id" not in kwargs:
-            self.id = str(uuid.uuid4())
-
-    def __str__(self):
-        """Returns a string representation of the instance"""
-        return "[Amenity] ({}) {}".format(self.id, self.__dict__)
