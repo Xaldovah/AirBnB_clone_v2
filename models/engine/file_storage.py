45c9993abb2fc739fe480740b41afc27f7c1007e
Dennis Kimani <dxaldovah@gmail.com>
2023-09-18 12:54:04 -0700

    returns the list of objects of one type of class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """Returns the list of objects of one type of class"""
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            filtered_objs = {}
+            for key, value in FileStorage.__objects.items():
+                if type(obj) == cls:
+                    filtered_objs[key] = obj
+            return filtered_objs
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +55,10 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it's inside"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in FileStorage.__objects:
+                del FileStorage.__objects[key]
